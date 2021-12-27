73ede88b21024078c2c2f08930fb60c4649efae1
JinkaiGUAN <pg121@ic.ac.uk>
2021-12-27 03:56:09 +0000

    calculator class

---
+++ 1-BasicSyntax/variablesAndDataType/Calculator.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.max;
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+
+        int maxNum = Math.max(a, b);
+        System.out.println(maxNum);
+    }
+
+}
---
+++ 1-BasicSyntax/variablesAndDataType/Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
