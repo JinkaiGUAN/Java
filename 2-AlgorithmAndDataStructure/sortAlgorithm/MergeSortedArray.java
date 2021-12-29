2bc52e293bdd19f3baeb5dfefd5bcd90fdb97b37
JinkaiGUAN <pg121@ic.ac.uk>
2021-12-29 12:07:44 +0000

    merge sorted arrays

--- 2-AlgorithmAndDataStructure/sortAlgorithm/MergeSortedArray.java
+++ 2-AlgorithmAndDataStructure/sortAlgorithm/MergeSortedArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author: Peter
  * @date: 29/12/2021
@@ -5,6 +7,36 @@
  */
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1+p2 - 1] = cur;
+        }
+        for (int i = 0; i != m + n; ++i) {
+            nums1[i] = sorted[i];
+        }
 
     }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {1, 2};
+        int m = 3, n = 2;
+
+        MergeSortedArray ma = new MergeSortedArray();
+        ma.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
 }
