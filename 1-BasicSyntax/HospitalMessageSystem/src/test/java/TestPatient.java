1ffdc975c7bb3db4d12cec92fdff6348e2501336
JinkaiGUAN <pg121@ic.ac.uk>
2021-12-30 11:54:15 +0000

    Test patient finished.

--- 1-BasicSyntax/HospitalMessageSystem/src/test/java/TestPatient.java
+++ 1-BasicSyntax/HospitalMessageSystem/src/test/java/TestPatient.java
@@ -1,6 +1,10 @@
 import DBEntities.Patient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * @author: Peter
@@ -8,11 +12,39 @@ import org.junit.jupiter.api.Assertions;
  * @description:
  */
 public class TestPatient {
+    Patient pat;
+    String name = new String("Martin");
+
+    @BeforeEach
+    public void setUP() {
+        pat = new Patient(name);
+    }
 
     @Test // why do we import Test from org.junit.jupiter.api.Test
     public void testWhereAmI() {
-        Patient pat = new Patient("Martin");
         String loc = pat.whereAmI();
         Assertions.assertEquals("Martin is in the ward 5.", loc);
+    }
+
+    @Test
+    public void testContact() {
+        String bedPhoneNum = "12455ada";
+        String msg = "Hello world!";
+
+        pat.setBedPhoneNum(bedPhoneNum);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream tempStore;
+
+        // Store current output stream
+        tempStore = System.out;
+        // Capture and replace System.out with my own output stream
+        System.setOut(new PrintStream(baos));
+        pat.contact(msg);
+        // Restore system.out
+        System.setOut(tempStore);
+
+        Assertions.assertEquals("Paging patient " + name + " on " + bedPhoneNum +  " Message: " + msg,
+                baos.toString().trim());
     }
 }
