573e7dd972370dd5bb07b9596c6b94176260431d
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-06 14:31:23 +0000

    Inheritance

---
+++ 2-SmallProjects/Phone/NewPhone.java
@@ -0,0 +1,10 @@
+
+public class NewPhone extends Phone {
+
+	@Override
+	public void show() {
+		super.show();
+		System.out.println("Shwo name");
+	}
+
+}
---
+++ 2-SmallProjects/Phone/Phone.java
@@ -0,0 +1,16 @@
+
+public class Phone {
+
+	public void call() {
+		System.out.println("Call");
+	}
+
+	public void send() {
+		System.out.println("Send");
+	}
+
+	public void show() {
+		System.out.println("Show the number");
+	}
+	
+}
