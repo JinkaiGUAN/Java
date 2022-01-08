17abaff24a001ccd14573875b4b01cdebcb3dee4
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-08 19:06:21 +0000

    Get next array in KMP

--- 3-AlgorithmAndDataStructure/StringLearning/src/ImplementstrStr.java
+++ 3-AlgorithmAndDataStructure/StringLearning/src/ImplementstrStr.java
@@ -24,7 +24,43 @@ public class ImplementstrStr {
     }
 
     public int strStrKMP(String haystack, String needle) {
+        // Here are are going to use KMP to implement this function, which can increase the working efficiency.
+        // KMP -> 字符串匹配问题
+
+        if (needle.length() == 0) return 0;
+
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+        int j = -1;
+        for(int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && haystack.charAt(i) != needle.charAt(j + 1)) {
+                j = next[j];
+            }
+            if (haystack.charAt(i) == needle.charAt(j + 1)) {
+                j++;
+            }
+            if (j == needle.length() - 1) {
+                return (i - needle.length() + 1);
+            }
+        }
 
         return -1;
     }
+
+    public void getNext(int[] next, String s) {
+        // 获取next数组， next数组：下标i之前（包括i）的字符串中， 有多大长度的相同前缀后缀
+        int j = -1;
+        next[0] = j;
+        for(int i = 0; i < s.length(); i++){
+            while (j >= 0 && s.charAt(i) != s.charAt(j+1)) {
+                j = next[j];
+            }
+
+            if (s.charAt(i) == s.charAt(j + 1)) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+
 }
