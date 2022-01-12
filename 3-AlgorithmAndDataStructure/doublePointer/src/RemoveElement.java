28a3504cc625311f525258df5cb4d0cbf64c641d
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-12 00:11:28 +0000

    Remove element

---
+++ 3-AlgorithmAndDataStructure/doublePointer/src/RemoveElement.java
@@ -0,0 +1,30 @@
+/**
+ * @author: Peter
+ * @date: 11/01/2022
+ * @description: https://leetcode.com/problems/remove-element/
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+//        if (nums == null || nums.length == 0) return 0;
+
+        int left = 0, right = nums.length;
+
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right-1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {3, 2, 2, 3};
+        int val = 3;
+
+        RemoveElement solution = new RemoveElement();
+        System.out.println(solution.removeElement(nums, val));
+    }
+}
