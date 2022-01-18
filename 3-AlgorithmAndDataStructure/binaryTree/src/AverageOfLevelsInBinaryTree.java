6cd2ca9d096caed3eb27d0a7d5e56ad7478666f0
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-18 22:53:44 +0000

    Average of lelvels in binary tree.

---
+++ 3-AlgorithmAndDataStructure/binaryTree/src/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: Peter
+ * @date: 18/01/2022
+ * @description: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+ */
+public class AverageOfLevelsInBinaryTree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> res = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Double sum = 0.0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                sum += (double) node.val;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(sum / size);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(3.0 / 2);
+    }
+}
