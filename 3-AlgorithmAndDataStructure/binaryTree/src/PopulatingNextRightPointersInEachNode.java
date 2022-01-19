e4e12d278152e88d52008223fa30bcfb17fdc292
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-19 09:58:00 +0000

    Populating next right pointers in each node

---
+++ 3-AlgorithmAndDataStructure/binaryTree/src/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,65 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+/**
+ * @author: Peter
+ * @date: 19/01/2022
+ * @description: https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class PopulatingNextRightPointersInEachNode {
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+            this.next = null;
+        }
+
+        public Node (int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+    }
+
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node pre = null;
+
+            for (int i = 0;  i < size; i++) {
+                Node cur = queue.remove();
+
+                if (pre != null) {
+                    pre.next = cur;
+                }
+                pre = cur;
+
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            // last node
+            pre.next = null;
+        }
+
+        return root;
+    }
+}
