d3566ffd6b2d8d540055ae3effc2bad101e454ed
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-20 09:34:28 +0000

    Min depth of the binary tree

--- 3-AlgorithmAndDataStructure/binaryTree/src/MinimumDepthOfBinaryTree.java
+++ 3-AlgorithmAndDataStructure/binaryTree/src/MinimumDepthOfBinaryTree.java
@@ -1,7 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author: Peter
  * @date: 20/01/2022
  * @description: https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
  */
 public class MinimumDepthOfBinaryTree {
+
+    public int minDepth(TreeNode root) {
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
 }
