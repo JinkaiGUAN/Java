e3b3615573e165abf15ebe8d8bf1ea5f3c4f9ca6
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-23 11:19:54 +0000

    Balance binary tree

---
+++ 3-AlgorithmAndDataStructure/binaryTree/src/BalancedBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: BalancedBinaryTree
+ * Author:   Peter
+ * Date:     23/01/2022 10:48
+ * Description:
+ * History:
+ * Version:
+ */
+public class BalancedBinaryTree {
+    // 因为求深度可以从上到下去查 所以需要前序遍历（中左右），而高度只能从下到上去查，所以只能后序遍历（左右中）
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = getHeight(root.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
