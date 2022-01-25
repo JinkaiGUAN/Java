2f76df096fe98502f2baacd113bc5748edac73ed
JinkaiGUAN <pg121@ic.ac.uk>
2022-01-25 11:18:21 +0000

    Construct binary tree from inorder and postorder traversal

---
+++ 3-AlgorithmAndDataStructure/binaryTree/src/ConstructBinaryTreeFromInorderAndPostOrderTraversal.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: ConstructBinaryTreeFromInorderAndPostOrderTraversal
+ * Author:   Peter
+ * Date:     25/01/2022 10:07
+ * Description: https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ * History:
+ * Version:
+ */
+public class ConstructBinaryTreeFromInorderAndPostOrderTraversal {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+        return getChild(inorder, 0, inorder.length, postorder, 0, postorder.length);
+    }
+
+
+    public TreeNode getChild(int[] inorder, int inLeft, int inRight, int[] postorder, int postLeft, int postRight) {
+        // 中断条件
+        if (inRight - inLeft < 1) {
+            return null;
+        }
+        if (inRight - inLeft == 1) {
+            return new TreeNode(inorder[inLeft]);
+        }
+
+        // 执行体
+        int rootVal = postorder[postRight - 1];
+        TreeNode root = new TreeNode(rootVal);
+
+        int inIdx = 0;
+        for (int i = inLeft ; i < inRight; i++) {
+            if (inorder[i] == rootVal) {
+                inIdx = i;
+                break;
+            }
+        }
+
+        TreeNode leftChild = getChild(inorder, inLeft, inIdx, postorder, postLeft, postLeft + (inIdx - inLeft));
+        TreeNode rightChild = getChild(inorder, inIdx + 1, inRight, postorder, postLeft + (inIdx - inLeft),
+                postRight -1);
+        root.left = leftChild;
+        root.right = rightChild;
+
+        return root;
+    }
+}
--- 3-AlgorithmAndDataStructure/binaryTree/src/FindBottomLeftTreeValue.java
+++ 3-AlgorithmAndDataStructure/binaryTree/src/FindBottomLeftTreeValue.java
@@ -20,7 +20,6 @@ public class FindBottomLeftTreeValue {
         }
 
         while (!queue.isEmpty()) {
-
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.remove();
