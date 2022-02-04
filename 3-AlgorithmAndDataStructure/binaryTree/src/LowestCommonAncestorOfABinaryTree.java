8b6f05baa41a535b90f5a2d509cc93c23fc3e5cc
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-04 11:06:57 +0000

    Lowest common ancestor of a binary tree

--- 3-AlgorithmAndDataStructure/binaryTree/src/LowestCommonAncestorOfABinaryTree.java
+++ 3-AlgorithmAndDataStructure/binaryTree/src/LowestCommonAncestorOfABinaryTree.java
@@ -9,7 +9,21 @@
  */
 public class LowestCommonAncestorOfABinaryTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
 
-        return null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null && right != null) {
+            return right;
+        } else if (left != null && right == null) {
+            return left;
+        } else if (left == null && right == null) {
+            return null;
+        } else {
+            return root;
+        }
     }
 }
