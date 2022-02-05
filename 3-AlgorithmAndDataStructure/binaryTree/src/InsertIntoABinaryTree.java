9f7ef115ec6325a506537232b3173be164c536f9
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-05 10:25:45 +0000

    Adding iteration method for inserting into BST

--- 3-AlgorithmAndDataStructure/binaryTree/src/InsertIntoABinaryTree.java
+++ 3-AlgorithmAndDataStructure/binaryTree/src/InsertIntoABinaryTree.java
@@ -21,4 +21,29 @@ public class InsertIntoABinaryTree {
 
         return root;
     }
+
+    public TreeNode insertIntoBSTIteration(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        TreeNode newRoot = root;
+        TreeNode pre = root;
+        while (root != null) {
+            pre = root;
+            if (root.val > val) {
+                root = root.left;
+            } else if (root.val < val) {
+                root = root.right;
+            }
+        }
+
+        if (pre.val > val) {
+            pre.left = new TreeNode(val);
+        } else {
+            pre.right = new TreeNode(val);
+        }
+
+        return newRoot;
+    }
 }
