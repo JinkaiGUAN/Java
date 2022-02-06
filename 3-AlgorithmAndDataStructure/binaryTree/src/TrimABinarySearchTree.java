5131a3f8da150a3daaf4c5bcbd8635681dc7fbce
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-06 10:18:06 +0000

    Trim a BST using iteration

--- 3-AlgorithmAndDataStructure/binaryTree/src/TrimABinarySearchTree.java
+++ 3-AlgorithmAndDataStructure/binaryTree/src/TrimABinarySearchTree.java
@@ -24,4 +24,39 @@ public class TrimABinarySearchTree {
 
         return root;
     }
+
+    public TreeNode trimBSTIteration(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+
+        // 处理头节点， 让它移动到合适位置
+        while (root != null && (root.val < low || root.val > high)) {
+            if (root.val < low) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
+        // 处理root左子树
+        TreeNode cur = root;
+        while (cur != null) {
+            while (cur.left != null && cur.left.val < low) {
+                cur.left = cur.left.right;
+            }
+            cur = cur.left;
+        }
+
+        // 处理root右子树
+        cur = root;
+        while (cur != null) {
+            while (cur.right != null && cur.right.val > high) {
+                cur.right = cur.right.left;
+            }
+            cur = cur.right;
+        }
+
+        return root;
+    }
 }
