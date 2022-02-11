d3a9c4708d8f9c227fef2b09af40630a164e8e08
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-11 18:13:33 +0000

    Finish the BST API

--- 4-DataStructureAPI/4-Trees/src/main/java/BinarySearchTree.java
+++ 4-DataStructureAPI/4-Trees/src/main/java/BinarySearchTree.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * Copyright (C), Peter GUAN
@@ -81,6 +83,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         // corresponding node.
 
         Node<Key, Value> node = delete(root, key);
+
     }
 
     /**
@@ -115,50 +118,269 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
             // The node has both left and right node
             // find the minimal node for the subtree.
-            Node<Key, Value> minNode = root;
-            while (minNode != null) {
+            Node<Key, Value> minNode = root.right;
+            while (minNode.left != null) {
                 minNode = minNode.left;
             }
 
+            // Delete the minimal node in the right subtree
+            Node<Key, Value> deleteNode = root.right;
+            while (deleteNode.left != null) {
+                if (deleteNode.left.left == null) {
+                    deleteNode.left = null;
+                }
+                deleteNode = deleteNode.left;
+            }
+
+            // Let the minNode be the new root, and the original left and right subtree kept the original structure.
+            minNode.left = root.left;
+            minNode.right = root.right;
+            root = minNode;
+
+            N--;
         }
 
         return root;
     }
 
     public Key min() {
-        return null;
+        return min(root).key;
     }
 
     private Node<Key, Value> min(Node<Key, Value> root) {
-        return null;
+        if (root.left != null) {
+            return min(root.left);
+        } else {
+            return root;
+        }
     }
 
     public Key max() {
-        return null;
+        return max(root).key;
     }
 
     private Node<Key, Value> max(Node<Key, Value> root) {
-        return null;
+        if (root.right != null) {
+            return max(root.right);
+        } else {
+            return root;
+        }
     }
 
     public int size() {
         return N;
     }
 
-    // The following parts are traversal method achievements, where recursion and iteration ideas are being used.
-    public Queue<Value> preorderTraversal() {
-        return null;
+    // The following parts are traversal method achievements, where recursion and iteration ideas are being used. In
+    // this part, we are going to output the keys.
+    public Queue<Key> preorderTraversal(boolean isIteration) {
+        if (isIteration) {
+            return preorderTraversalIterative();
+        } else {
+            // do the recursion preorder traversal
+            Queue<Key> keys = new LinkedList<>();
+            preorderTraversalRecursion(root, keys);
+            return keys;
+        }
     }
 
-    public Queue<Value> inorderTraversal() {
-        return null;
+    private Queue<Key> preorderTraversalIterative() {
+        Queue<Key> res = new LinkedList<>();
+        Stack<Node<Key, Value>> stack = new Stack<>();
+
+        // In this iterative method, we are going to add null to identify whether we add this node into our res queue
+        // or not.
+        if (root != null) {
+            stack.push(root);
+        }
+
+        while (!stack.isEmpty()) {
+            Node<Key, Value> node = stack.pop();
+
+            if (node != null) {
+                // Now we have not visited this node
+
+                // right
+                if (node.right != null) {
+                    stack.push(root.right);
+                }
+                // left
+                if (node.left != null) {
+                    stack.push(root.left);
+                }
+                // root
+                stack.push(node);
+                stack.push(null);
+
+            } else {
+                // we have visited the node before this node
+                node = stack.pop();
+                res.add(node.key);
+            }
+        }
+
+        return res;
     }
 
-    public Queue<Value> postorderTraversal() {
-        return null;
+    private void preorderTraversalRecursion(Node<Key, Value> root, Queue<Key> keys) {
+        if (root == null) {
+            return;
+        }
+
+        // root -> left -> right
+        keys.offer(root.key);
+        if (root.left != null) {
+            preorderTraversalRecursion(root.left, keys);
+        }
+        if (root.right != null) {
+            preorderTraversalRecursion(root.right, keys);
+        }
     }
 
-    public Queue<Value> levelTraversal() {
-        return null;
+    public Queue<Key> inorderTraversal(boolean isIteration) {
+        if (isIteration) {
+            return inorderTraversalIteration();
+        } else {
+            Queue<Key> keys = new LinkedList<>();
+            inorderTraversalRecursion(root, keys);
+            return keys;
+        }
+    }
+
+    private Queue<Key> inorderTraversalIteration() {
+        Queue<Key> res = new LinkedList<>();
+        Stack<Node<Key, Value>> stack = new Stack<>();
+
+        // In this iterative method, we are going to add null to identify whether we add this node into our res queue
+        // or not.
+        if (root != null) {
+            stack.push(root);
+        }
+
+        while (!stack.isEmpty()) {
+            Node<Key, Value> node = stack.pop();
+
+            if (node != null) {
+                // Now we have not visited this node
+
+                // right
+                if (node.right != null) {
+                    stack.push(root.right);
+                }
+                // root
+                stack.push(node);
+                stack.push(null);
+                // left
+                if (node.left != null) {
+                    stack.push(root.left);
+                }
+            } else {
+                // we have visited the node before this node
+                node = stack.pop();
+                res.add(node.key);
+            }
+        }
+
+        return res;
+    }
+
+    private void inorderTraversalRecursion(Node<Key, Value> root, Queue<Key> keys) {
+        if (root == null) {
+            return;
+        }
+
+        // left -> root -> right
+        if (root.left != null) {
+            inorderTraversalRecursion(root.left, keys);
+        }
+        keys.offer(root.key);
+        if (root.right != null) {
+            inorderTraversalRecursion(root.right, keys);
+        }
+    }
+
+    public Queue<Key> postorderTraversal(boolean isIteration) {
+        if (isIteration) {
+            return null;
+        } else {
+            Queue<Key> keys = new LinkedList<>();
+            postorderTraversalRecursion(root, keys);
+            return keys;
+        }
+    }
+
+    private Queue<Key> postorderTraversalIteration() {
+        Queue<Key> res = new LinkedList<>();
+        Stack<Node<Key, Value>> stack = new Stack<>();
+
+        // In this iterative method, we are going to add null to identify whether we add this node into our res queue
+        // or not.
+        if (root != null) {
+            stack.push(root);
+        }
+
+        while (!stack.isEmpty()) {
+            Node<Key, Value> node = stack.pop();
+
+            if (node != null) {
+                // Now we have not visited this node
+
+                // root
+                stack.push(node);
+                stack.push(null);
+                // right
+                if (node.right != null) {
+                    stack.push(root.right);
+                }
+                // left
+                if (node.left != null) {
+                    stack.push(root.left);
+                }
+            } else {
+                // we have visited the node before this node
+                node = stack.pop();
+                res.add(node.key);
+            }
+        }
+
+        return res;
+    }
+
+    private void postorderTraversalRecursion(Node<Key, Value> root, Queue<Key> keys) {
+        if (root == null) {
+            return;
+        }
+
+        // left -> right -> root
+        if (root.left != null) {
+            postorderTraversalRecursion(root.left, keys);
+        }
+        if (root.right != null) {
+            postorderTraversalRecursion(root.right, keys);
+        }
+        keys.offer(root.key);
+    }
+
+    public Queue<Key> levelTraversal() {
+        Queue<Key> res = new LinkedList<>();
+        Queue<Node<Key, Value>> levels = new LinkedList<>();
+
+        if (root != null) {
+            levels.offer(root);
+        }
+
+        while (!levels.isEmpty()) {
+            Node<Key, Value> node = levels.poll();
+            res.offer(node.key);
+
+            if (node.left != null) {
+                levels.offer(node.left);
+            }
+            if (node.right != null) {
+                levels.offer(node.right);
+            }
+        }
+
+        return res;
     }
 }
