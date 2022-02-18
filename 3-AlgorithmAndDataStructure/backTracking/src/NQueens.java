6420efa1f607d7a777d3be54f100369289a0c878
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-18 20:30:13 +0000

    solve sudoku

--- 3-AlgorithmAndDataStructure/backTracking/src/NQueens.java
+++ 3-AlgorithmAndDataStructure/backTracking/src/NQueens.java
@@ -77,6 +77,4 @@ public class NQueens {
         return true;
     }
 
-
-
 }
--- 3-AlgorithmAndDataStructure/backTracking/src/SudokuSolver.java
+++ 3-AlgorithmAndDataStructure/backTracking/src/SudokuSolver.java
@@ -8,5 +8,62 @@
  * Version:
  */
 public class SudokuSolver {
+    public void solveSudoku(char[][] board) {
+        solveSudokuHelper(board);
+    }
 
+    private boolean solveSudokuHelper(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            // 遍历行
+            for (int j = 0; j < 9; j++) {
+                // iterate the value in col
+                if (board[i][j] != '.') {
+                    continue;
+                }
+                for (char k = '1'; k <= '9'; k++) {
+                    if (isValid(i, j, k, board)) {
+                        board[i][j] = k;
+                        if (solveSudokuHelper(board)) {
+                            return true;
+                        }
+                        board[i][j] = '.';
+                    }
+                }
+
+                // all numbers have been tested, and there is no suitable one, so return false
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(int row, int col, char val, char[][] chessboard) {
+        // same row
+        for (int i = 0; i < 9; i++) {
+            if (chessboard[row][i] == val) {
+                return false;
+            }
+        }
+
+        // same col
+        for (int i = 0; i < 9; i++) {
+            if (chessboard[i][col] == val) {
+                return false;
+            }
+        }
+
+        // In the 3 by 3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (chessboard[i][j] == val) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
