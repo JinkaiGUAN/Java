ba7a9bfa9a94367f83647a4260d3df19ae2aa487
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-23 23:11:26 +0000

    candy

---
+++ 3-AlgorithmAndDataStructure/greedyAlgo/src/candy.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: candy
+ * Author:   Peter
+ * Date:     23/02/2022 23:02
+ * Description: https://leetcode-cn.com/problems/candy/
+ * History:
+ * Version:
+ */
+public class candy {
+    public int candy(int[] ratings) {
+        // assign one for each person
+        int[] candies = new int[ratings.length];
+        for (int i = 0; i < ratings.length; i++) {
+            candies[i] = 1;
+        }
+
+        // rank children from left to right
+        for (int i = 1; i < ratings.length; i++) {
+            // fixme： what if the ratings of two children are the same
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // rank the children from right ro left
+        for (int i = ratings.length - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i]) {
+                candies[i - 1] = Math.max(candies[i - 1], candies[i] + 1);
+            }
+        }
+
+        // count total candies
+        int total = 0;
+        for (int candy : candies) {
+            total += candy;
+        }
+
+        return total;
+    }
+}
