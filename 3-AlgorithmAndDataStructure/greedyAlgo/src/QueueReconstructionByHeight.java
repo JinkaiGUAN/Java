5ec6f565aea3bafcc3cecc3b41600c64574b69b0
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-24 00:13:23 +0000

    Queue reconstruction by height

---
+++ 3-AlgorithmAndDataStructure/greedyAlgo/src/QueueReconstructionByHeight.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: QueueReconstructionByHeight
+ * Author:   Peter
+ * Date:     23/02/2022 23:24
+ * Description: https://leetcode-cn.com/problems/queue-reconstruction-by-height/
+ * History:
+ * Version:
+ *
+ * Explanation
+ *
+ */
+public class QueueReconstructionByHeight {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1]; // 相同身高， 次数小的排在前面
+            return b[0] - a[0];  // 按照身高排序， 身高高的放在前面
+        });
+
+        LinkedList<int[]> que = new LinkedList<>();
+
+        for (int[] p : people) {
+            que.add(p[1], p);
+        }
+
+        return que.toArray(new int[people.length][]);
+    }
+}
