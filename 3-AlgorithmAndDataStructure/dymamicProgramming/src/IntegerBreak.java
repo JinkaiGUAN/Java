e05b7419edc55c9bea1403b53e47573f601f6f13
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-27 12:43:41 +0000

    Integer break

---
+++ 3-AlgorithmAndDataStructure/dymamicProgramming/src/IntegerBreak.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: IntegerBreak
+ * Author:   Peter
+ * Date:     27/02/2022 12:16
+ * Description: https://leetcode-cn.com/problems/integer-break/
+ * History:
+ * Version:
+ */
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            for(int j = 1; j < i - 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+
+        return dp[n];
+    }
+}
