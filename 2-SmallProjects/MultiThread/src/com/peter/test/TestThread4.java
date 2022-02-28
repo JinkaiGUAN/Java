4c417c0f53603d482762bf64ed794a95abe6192b
JinkaiGUAN <pg121@ic.ac.uk>
2022-02-28 23:33:47 +0000

    using multiple thread to control one object

---
+++ 2-SmallProjects/MultiThread/src/com/peter/test/TestThread4.java
@@ -0,0 +1,38 @@
+package com.peter.test;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: TestThread4
+ * Author:   Peter
+ * Date:     28/02/2022 23:20
+ * Description: Using multiple threads to control one object
+ * History:
+ * Version:
+ */
+public class TestThread4 implements Runnable {
+    private int ticketNum = 10;
+
+    @Override
+    public void run() {
+        // 此处同时操作 ticketNum 会引起资源不安全
+        while (true) {
+            if (ticketNum <= 0) {
+                break;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " gets ticket num " + ticketNum--);
+        }
+    }
+
+    public static void main(String[] args) {
+        TestThread4 t1 = new TestThread4();
+
+        new Thread(t1, "Sim").start();
+        new Thread(t1, "john").start();
+        new Thread(t1, "Eva").start();
+    }
+}
