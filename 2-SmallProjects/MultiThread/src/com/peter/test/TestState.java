d4d8c839c00a7cb343f7eb912024388b23545b0e
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-01 10:31:13 +0000

    Get the state of state

---
+++ 2-SmallProjects/MultiThread/src/com/peter/test/TestState.java
@@ -0,0 +1,46 @@
+package com.peter.test;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: TestState
+ * Author:   Peter
+ * Date:     01/03/2022 10:24
+ * Description:
+ * History:
+ * Version:
+ */
+public class TestState {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("------------");
+        });
+
+        // new
+        Thread.State state = thread.getState();
+        System.out.println(state);
+
+        // run
+        thread.start();
+        state = thread.getState();
+        System.out.println(state);
+
+        // blocked or terminated
+        while (state != Thread.State.TERMINATED) {
+            try {
+                Thread.sleep(100);
+                state = thread.getState();
+                System.out.println(state);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
