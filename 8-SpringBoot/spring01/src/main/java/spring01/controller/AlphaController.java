2690458541d56997abff5a33713c2a8c83ef41d6
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-08 21:13:55 +0000

    Two updating parameters method

--- 8-SpringBoot/spring01/src/main/java/spring01/controller/AlphaController.java
+++ 8-SpringBoot/spring01/src/main/java/spring01/controller/AlphaController.java
@@ -2,9 +2,14 @@ package spring01.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import spring01.service.AlphaService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /**
  * Copyright (C), Peter GUAN
@@ -33,6 +38,53 @@ public class AlphaController {
     @ResponseBody
     public String getData(){
         return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        // 获取请求数据
+        System.out.println("method ---- " + request.getMethod());
+        System.out.println("Servlet path ---- " + request.getServletPath()); // 请求路径
+        Enumeration<String> names = request.getHeaderNames();
+        while(names.hasMoreElements()) {
+            String name = names.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + " : " + value);
+        }
+        System.out.println(request.getParameter("code")); // 拿到浏览器请求的参数
+
+        // 返回相应数据
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            writer.write("<h1>NewCoder</h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+
+    // 两种传参的方式
+    // Get request : 检测url为 /students?current=1&limit=20 的网页
+    @RequestMapping(path="/students", method = RequestMethod.GET)
+    @ResponseBody
+    public String  getStudents(@RequestParam(name = "current", required = false, defaultValue = "1") int current,
+                               @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        // 写的input variable必须于网页给定的参数一致
+        System.out.println("current : " + current + " limit: " + limit);
+        return "Students";
+    }
+
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        System.out.println("ID : " + id);
+        return "A student";
     }
 
 }
