2ef30bc2d8f1b50ebbb201cca951c0ac836b3721
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-16 11:21:05 +0000

    House robber III

--- 3-AlgorithmAndDataStructure/dymamicProgramming/src/HouseRobberIII.java
+++ 3-AlgorithmAndDataStructure/dymamicProgramming/src/HouseRobberIII.java
@@ -10,7 +10,26 @@
 public class HouseRobberIII {
 
     public int rob(TreeNode root) {
+        int[] res = robAction(root);
 
+        return Math.max(res[0], res[1]);
+    }
+
+    public int[] robAction(TreeNode root) {
+        // dp[0] 表示不偷该节点， dp[1]表示偷该节点
+        int[] res = new int[2];
+
+        if (root == null) {
+            return res;
+        }
+
+        int[] left = robAction(root.left);
+        int[] right = robAction(root.right);
+
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = root.val + left[0] + right[0];
+
+        return res;
     }
 }
 
