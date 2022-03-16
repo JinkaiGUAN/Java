36166c664789935dfa38967cb924b2748f93f777
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-16 09:43:23 +0000

    Upload header in user setting

--- 8-SpringBoot/spring01/src/main/java/spring01/controller/UserController.java
+++ 8-SpringBoot/spring01/src/main/java/spring01/controller/UserController.java
@@ -1,8 +1,26 @@
 package spring01.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import spring01.entity.User;
+import spring01.service.UserService;
+import spring01.util.CommunityUtil;
+import spring01.util.HostHolder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Copyright (C), Peter GUAN
@@ -18,10 +36,96 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/user")
 public class UserController {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
     public String getSettingPage() {
         return "/site/setting";
     }
 
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if (headerImage == null) {
+            model.addAttribute("error", "Have not select any images yet");
+            return "/site/setting";
+        }
 
+        // 判断文件后缀
+        String filename = headerImage.getOriginalFilename();
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        if (StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "Wrong  image format!");
+            return "/site/setting";
+        }
+
+        // 生成随机文件名
+        filename = CommunityUtil.generateUUID() + suffix;
+        File dest = new File(uploadPath + "/" + filename); // 文件存放路径
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("Unsuccessfully upload: " + e.getMessage());
+            throw new RuntimeException("上传文件失败， 服务器异常", e);
+        }
+
+        // update the header url (web 访问路径)
+        // http:localhost:8080/community/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + filename;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    /**
+     * 返回名字 http:localhost:8080/community/user/header/xxx.png
+     * @param filename
+     * @param response
+     */
+    @RequestMapping(path = "/header/{filename}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("filename") String filename, HttpServletResponse response) {
+        // 服务器存放路径
+        String filepath = uploadPath + "/" + filename;
+        String suffix = filename.substring(filename.lastIndexOf("."));
+        // 响应图片
+        response.setContentType("image/" + suffix);
+
+        FileInputStream fis = null;
+        try {
+            OutputStream os = response.getOutputStream(); // spring 可自动关闭
+            fis = new FileInputStream(filepath);
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            logger.error("读取图像失败" + e.getMessage());
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 }
