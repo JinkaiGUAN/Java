b31ac8549a4e55b5519925657765810a50353db2
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-17 22:35:09 +0000

    Add post details

--- 8-SpringBoot/spring01/src/main/java/spring01/controller/DiscussPostController.java
+++ 8-SpringBoot/spring01/src/main/java/spring01/controller/DiscussPostController.java
@@ -2,12 +2,15 @@ package spring01.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import spring01.entity.DiscussPost;
 import spring01.entity.User;
 import spring01.service.DiscussPostService;
+import spring01.service.UserService;
 import spring01.util.CommunityUtil;
 import spring01.util.HostHolder;
 
@@ -31,7 +34,15 @@ public class DiscussPostController {
     private DiscussPostService discussPostService;
     @Autowired
     private HostHolder hostHolder; // 获取当前用户
+    @Autowired
+    private UserService userService;
 
+    /**
+     * 发布帖子
+     * @param title
+     * @param content
+     * @return
+     */
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title, String content){
@@ -52,6 +63,27 @@ public class DiscussPostController {
         // 报错的情况 将来统一处理
         return CommunityUtil.getJSONString(0, "发布成功！");
     }
+
+    /**
+     * 查询帖子详情
+     * @param discussPostId
+     * @param model
+     * @return
+     */
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 查询帖子信息
+        DiscussPost discussPost = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", discussPost); // 在页面使用${post.title} 会自动调用响应的get方法
+        // 作者信息
+        User user = userService.findUserById(discussPostId);
+        model.addAttribute("user", user);
+
+        //todo: 回复功能
+
+        return "/site/discuss-detail";
+    }
+
 
 
 }
--- 8-SpringBoot/spring01/src/main/java/spring01/service/DiscussPostService.java
+++ 8-SpringBoot/spring01/src/main/java/spring01/service/DiscussPostService.java
@@ -49,4 +49,8 @@ public class DiscussPostService {
 
         return discussPostMapper.insertDiscussPost(discussPost);
     }
+
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
