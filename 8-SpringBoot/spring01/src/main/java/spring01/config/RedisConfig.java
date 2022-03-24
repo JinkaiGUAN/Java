88dede5e6e437bfebdb0e0e2bb3bf7b06381819b
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-24 11:20:26 +0000

    Add redis configuration and test its functions

---
+++ 8-SpringBoot/spring01/src/main/java/spring01/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package spring01.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: RedisConfig
+ * Author:   Peter
+ * Date:     24/03/2022 10:15
+ * Description:
+ * History:
+ * Version:
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
---
+++ 8-SpringBoot/spring01/src/test/java/spring01/RedisTests.java
@@ -0,0 +1,143 @@
+package spring01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: RedisTests
+ * Author:   Peter
+ * Date:     24/03/2022 10:44
+ * Description:
+ * History:
+ * Version:
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = Spring01Application.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings() {
+        String redisKey = "test:count";
+
+        redisTemplate.opsForValue().set(redisKey, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHash() {
+        String redisKey = "test:user";
+
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "name", "John");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "name"));
+    }
+
+    @Test
+    public void testLists() {
+        String redisKey = "test:ids";
+
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().leftPush(redisKey, 103);
+
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey, 0));
+        System.out.println(redisTemplate.opsForList().range(redisKey, 1, 2));
+
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+    }
+
+    @Test
+    public void testSets() {
+        String redisKey = "test:teachers";
+
+        redisTemplate.opsForSet().add(redisKey, "John", "HKG", "haud", "haoidhoa");
+
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+    }
+
+    @Test
+    public void testSortedSets() {
+        String redisKey = "test:students2";
+
+        redisTemplate.opsForZSet().add(redisKey, "John", 80);
+        redisTemplate.opsForZSet().add(redisKey, "John1", 90);
+        redisTemplate.opsForZSet().add(redisKey, "John2", 50);
+        redisTemplate.opsForZSet().add(redisKey, "John3", 70);
+        redisTemplate.opsForZSet().add(redisKey, "John4", 60);
+
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey, "John1"));
+        System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey, "John1"));
+        System.out.println(redisTemplate.opsForZSet().reverseRange(redisKey, 0, 2));
+    }
+
+    @Test
+    public void testKeys() {
+        redisTemplate.delete("test:students");
+
+        System.out.println(redisTemplate.hasKey("test:students"));
+
+        redisTemplate.expire("test:students2", 10, TimeUnit.SECONDS);
+    }
+
+    // 多次访问同一个key
+    @Test
+    public void testBoundOperations() {
+        String redisKey = "test:count";
+
+        BoundValueOperations operations  = redisTemplate.boundValueOps(redisKey);
+
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    // 编程式事务
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+
+                operations.multi();
+
+                operations.opsForSet().add(redisKey, "jsn");
+                operations.opsForSet().add(redisKey, "jsn1");
+                operations.opsForSet().add(redisKey, "jsn2");
+
+                System.out.println(operations.opsForSet().members(redisKey));
+
+                return operations.exec();
+            }
+        });
+
+        System.out.println(obj);
+    }
+}
