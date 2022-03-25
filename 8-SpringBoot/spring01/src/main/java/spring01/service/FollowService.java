3fbe0fc41d743ec9bfcc67693861e738c8688d48
JinkaiGUAN <pg121@ic.ac.uk>
2022-03-25 18:52:33 +0000

    Add comment service

---
+++ 8-SpringBoot/spring01/src/main/java/spring01/service/FollowService.java
@@ -0,0 +1,61 @@
+package spring01.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.stereotype.Service;
+import spring01.util.RedisKeyUtil;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: FollowService
+ * Author:   Peter
+ * Date:     25/03/2022 18:45
+ * Description:
+ * History:
+ * Version:
+ */
+
+@Service
+public class FollowService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public void follow(int userId, int entityType, int entityId) {
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+
+                operations.opsForZSet().add(followeeKey, entityId, System.currentTimeMillis());
+                operations.opsForZSet().add(followerKey, userId, System.currentTimeMillis());
+
+                return operations.exec();
+            }
+        });
+    }
+
+    public void unfollow(int userId, int entityType, int entityId) {
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+                // 一次执行多次操作， 要保证数据ACID
+                operations.opsForZSet().remove(followeeKey, entityId);
+                operations.opsForZSet().remove(followerKey, userId);
+
+                return operations.exec();
+            }
+        });
+    }
+}
