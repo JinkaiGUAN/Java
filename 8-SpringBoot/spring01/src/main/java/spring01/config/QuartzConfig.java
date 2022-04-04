ab38f3e9075f2671612f819de42d077bf34239d7
JinkaiGUAN <pg121@ic.ac.uk>
2022-04-04 21:03:47 +0100

    Log issue

---
+++ 8-SpringBoot/spring01/src/main/java/spring01/config/QuartzConfig.java
@@ -0,0 +1,63 @@
+package spring01.config;
+
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+import spring01.quartz.AlphaJob;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: QuartzConfig
+ * Author:   Peter
+ * Date:     04/04/2022 16:00
+ * Description: 在第一次使用时被初始化到数据苦衷， 之后每次都使用数据库
+ * History:
+ * Version:
+ * @author Peter
+ */
+
+@Configuration
+public class QuartzConfig {
+    // FactoryBean可简化Bean的实例化过程:
+    // 1.通过FactoryBean封装Bean的实例化过程.
+    // 2.将FactoryBean装配到Spring容器里.
+    // 3.将FactoryBean注入给其他的Bean.
+    // 4.该Bean得到的是FactoryBean所管理的对象实例.
+
+
+    /**
+     * 配置JobDetail
+     * @return
+     */
+    //@Bean
+    public JobDetailFactoryBean alphaJobDetail() {
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setJobClass(AlphaJob.class);
+        factoryBean.setName("alphaJob");
+        factoryBean.setGroup("alphaJobGroup");
+        factoryBean.setDurability(true);
+        factoryBean.setRequestsRecovery(true);
+        return factoryBean;
+    }
+
+    /**
+     *  配置Trigger(SimpleTriggerFactoryBean, CronTriggerFactoryBean)
+     * @param alphaJobDetail
+     * @return
+     */
+    //@Bean
+    public SimpleTriggerFactoryBean alphaTrigger(JobDetail alphaJobDetail) {
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(alphaJobDetail);
+        factoryBean.setName("alphaTrigger");
+        factoryBean.setGroup("alphaTriggerGroup");
+        factoryBean.setRepeatInterval(3000);
+        factoryBean.setJobDataMap(new JobDataMap());
+        return factoryBean;
+    }
+
+
+}
