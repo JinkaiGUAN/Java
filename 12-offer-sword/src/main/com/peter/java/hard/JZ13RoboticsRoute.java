ca00e1da56c54b4135760277b34108e62da8d2d3
JinkaiGUAN <pg121@ic.ac.uk>
2022-04-17 21:02:21 +0100

    Get the maximum product of substrings using recursion.

---
+++ 12-offer-sword/src/main/com/peter/java/hard/JZ13RoboticsRoute.java
@@ -0,0 +1,17 @@
+package main.com.peter.java.hard;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: JZ13RoboticsRoute
+ * Author:   Peter
+ * Date:     17/04/2022 20:21
+ * Description: https://www.nowcoder.com/practice/6e5207314b5241fb83f2329e89fdecc8?tpId=265&tags=&title=&difficulty=0&judgeStatus=0&rp=1&sourceUrl=%2Fexam%2Foj%2Fta%3FtpId%3D13
+ * History:
+ * Version:
+ *
+ * @author: Peter
+ */
+public class JZ13RoboticsRoute {
+
+
+}
