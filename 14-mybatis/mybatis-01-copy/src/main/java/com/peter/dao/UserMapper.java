da92be4639dd6a6ca897f0eaaedfb4bfb43994df
JinkaiGUAN <pg121@ic.ac.uk>
2022-04-19 22:39:47 +0100

    rebuild mybatis framework

---
+++ 14-mybatis/mybatis-01-copy/src/main/java/com/peter/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package com.peter.dao;
+
+import com.peter.pojo.User;
+
+import java.util.List;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: UserMapper
+ * Author:   Peter
+ * Date:     19/04/2022 22:34
+ * Description:
+ * History:
+ * Version:
+ */
+public interface UserMapper {
+
+    List<User> getUserList();
+}
