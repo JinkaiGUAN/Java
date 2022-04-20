ef803a23da4764313473c1279c69a69eaa31818b
JinkaiGUAN <pg121@ic.ac.uk>
2022-04-20 09:14:36 +0100

    The sub tree

---
+++ 12-offer-sword/src/main/com/peter/java/entity/TreeNode.java
@@ -0,0 +1,21 @@
+package main.com.peter.java.entity;
+
+/**
+ * Copyright (C), Peter GUAN
+ * FileName: TreeNode
+ * Author:   Peter
+ * Date:     20/04/2022 08:28
+ * Description:
+ * History:
+ * Version:
+ */
+public class TreeNode {
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
