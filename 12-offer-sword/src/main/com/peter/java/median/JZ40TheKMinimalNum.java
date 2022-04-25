c1a43dd75af6a3a410a063efa1214f1aeeabd0b6
JinkaiGUAN <pg121@ic.ac.uk>
2022-04-25 22:30:28 +0100

    Using priority queue to sort the array

--- 12-offer-sword/src/main/com/peter/java/median/JZ40TheKMinimalNum.java
+++ 12-offer-sword/src/main/com/peter/java/median/JZ40TheKMinimalNum.java
@@ -3,6 +3,7 @@ package main.com.peter.java.median;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 /**
  * Copyright (C), Peter GUAN
@@ -15,7 +16,7 @@ import java.util.Collections;
  * Version:
  */
 public class JZ40TheKMinimalNum {
-    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
         ArrayList<Integer> res = new ArrayList<>();
 
         Arrays.sort(input);
@@ -26,11 +27,29 @@ public class JZ40TheKMinimalNum {
         return res;
     }
 
-    private void sortReverse(int[] array) {
-        // method 1
-        //Arrays.sort(new int[][]{array}, Collections.reverseOrder());
-        //
-        //// method 2
-        //Arrays.sort(array, (o1, o2) -> {return o2 - o1;});
+    public ArrayList<Integer> GetLeastNumbers_Solution2(int[] input, int k) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        //排除特殊情况
+        if (k == 0 || input.length == 0) {
+            return res;
+        }
+        //大根堆
+        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1));
+        //构建一个k个大小的堆
+        for (int i = 0; i < k; i++) {
+            q.offer(input[i]);
+        }
+        for (int i = k; i < input.length; i++) {
+            //较小元素入堆
+            if (q.peek() > input[i]) {
+                q.poll();
+                q.offer(input[i]);
+            }
+        }
+        //堆中元素取出入数组
+        for (int i = 0; i < k; i++) {
+            res.add(q.poll());
+        }
+        return res;
     }
 }
